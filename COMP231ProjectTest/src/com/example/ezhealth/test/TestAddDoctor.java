c821cbb0185297bdc4561fbb982696c3284654e6
300735227 <sukhvir_gill2001@yahoo.com>
2013-12-05 01:09:06 -0500

    UnitTesting Patient Registration
    unit testing on patient registration form to check all the validations
    and input values.

---
+++ COMP231ProjectTest/src/com/example/ezhealth/test/TestAddDoctor.java
@@ -0,0 +1,134 @@
+package com.example.ezhealth.test;
+
+import com.example.ezhealth.AddDoctor;
+import com.example.ezhealth.AddReceptionist;
+
+import junit.framework.TestCase;
+
+public class TestAddDoctor extends TestCase {
+
+public void testConstructorRecp(){
+		
+		AddDoctor i = new AddDoctor();
+		assertNotNull(i);
+	}
+	
+	public boolean testEmailValidatiionCheckValid()
+	{
+		boolean check=false;
+		String myEmail="manvir@yahoo.com";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkEmailCorrect(myEmail);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testEmailValidatiionCheckInvalid()
+	{
+		boolean check=false;
+		String myEmail="manvir@yacom";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkEmailCorrect(myEmail);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckPhoneCorrectValid()
+	{
+		boolean check=false;
+		String myPhone="1234567890";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkPhoneCorrect(myPhone);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckPhoneCorrectInvalid()
+	{
+		boolean check=false;
+		String myPhone="12345678";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkPhoneCorrect(myPhone);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckDOBCorrectValid()
+	{
+		boolean check=false;
+		String myDOB="17/05/1990";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkDOBCorrect(myDOB);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckDOBCorrectInvalid()
+	{
+		boolean check=false;
+		String myDOB="05/17/1990";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkDOBCorrect(myDOB);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckFnameCorrectValid()
+	{
+		boolean check=false;
+		String myFname="Manvir";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkFnameCorrect(myFname);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckFnameCorrectInvalid()
+	{
+		boolean check=false;
+		String myFname="Manvir234";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkFnameCorrect(myFname);
+		assertFalse(check);
+		
+		return check;
+	}
+	public boolean testcheckLnameCorrectValid()
+	{
+		boolean check=false;
+		String myLname="Dhaliwal";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkLnameCorrect(myLname);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckLnameCorrectInvalid()
+	{
+		boolean check=false;
+		String myLname="Dhaliwal123";
+		AddDoctor aR=new AddDoctor();
+		assertNotNull(aR);
+		check=aR.checkLnameCorrect(myLname);
+		assertFalse(check);
+		
+		return check;
+	}
+}
---
+++ COMP231ProjectTest/src/com/example/ezhealth/test/TestPatientRegistration.java
@@ -0,0 +1,135 @@
+package com.example.ezhealth.test;
+
+
+import com.example.ezhealth.PatientRegistration;
+
+import junit.framework.TestCase;
+
+public class TestPatientRegistration extends TestCase {
+
+public void testConstructorRecp(){
+		
+	PatientRegistration i = new PatientRegistration();
+		assertNotNull(i);
+	}
+	
+	public boolean testEmailValidatiionCheckValid()
+	{
+		boolean check=false;
+		String myEmail="sukh@yahoo.com";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkEmailCorrect(myEmail);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testEmailValidatiionCheckInvalid()
+	{
+		boolean check=false;
+		String myEmail="sukh@yahoocom";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkEmailCorrect(myEmail);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckPhoneCorrectValid()
+	{
+		boolean check=false;
+		String myPhone="1234567890";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkPhoneCorrect(myPhone);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckPhoneCorrectInvalid()
+	{
+		boolean check=false;
+		String myPhone="12345678";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkPhoneCorrect(myPhone);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckDOBCorrectValid()
+	{
+		boolean check=false;
+		String myDOB="17/05/1990";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkDOBCorrect(myDOB);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckDOBCorrectInvalid()
+	{
+		boolean check=false;
+		String myDOB="05/17/1990";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkDOBCorrect(myDOB);
+		assertFalse(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckFnameCorrectValid()
+	{
+		boolean check=false;
+		String myFname="Sukh";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkFnameCorrect(myFname);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckFnameCorrectInvalid()
+	{
+		boolean check=false;
+		String myFname="Sukh123";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkFnameCorrect(myFname);
+		assertFalse(check);
+		
+		return check;
+	}
+	public boolean testcheckLnameCorrectValid()
+	{
+		boolean check=false;
+		String myLname="Gill";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkLnameCorrect(myLname);
+		assertTrue(check);
+		
+		return check;
+	}
+	
+	public boolean testcheckLnameCorrectInvalid()
+	{
+		boolean check=false;
+		String myLname="Gill123";
+		PatientRegistration aR=new PatientRegistration();
+		assertNotNull(aR);
+		check=aR.checkLnameCorrect(myLname);
+		assertFalse(check);
+		
+		return check;
+	}
+
+}
